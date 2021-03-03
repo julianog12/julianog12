853f555e1865209c5c6e3153a57669d2ed88e086
Juliano <julianog12@hotmail.com>
2021-03-03 13:39:04 -0300

    Graficos Totais

--- app/controllers/painel_controller.rb
+++ app/controllers/painel_controller.rb
@@ -5,6 +5,29 @@ class PainelController < ApplicationController
 
     @total_linhas = 0
     tot_linhas = 0
+
+    linhas_por_modelo = []
+    Funcao.select('substring(cd_componente,1,4) as modelo')
+           .where('cd_empresa = ? and length(cd_componente) = 8', "#{params[:cd_empresa]}").group("substring(cd_componente,1,4)").each do |reg|
+      Funcao.where('substring(cd_componente,1,4) = ? and cd_empresa = ? and length(cd_componente) = 8', reg.modelo, "#{params[:cd_empresa]}").select('codigo').each do |regt|
+        tot_linhas = regt.codigo.count("\n")
+        if !linhas_por_modelo.nil? 
+          if linhas_por_modelo.find {|x| x[:name] == reg.modelo}.nil? 
+            linhas_por_modelo << { name: reg.modelo, data: tot_linhas }
+          else
+            linhas_por_modelo.find{|h| h[:name] == reg.modelo}[:data] += tot_linhas
+          end
+        else
+          linhas_por_modelo << { name: reg.modelo, data: tot_linhas }
+        end
+     end
+    end
+    linhas_por_modelo.first(25)
+    @tot_linhas_por_modelo = {}
+    linhas_por_modelo.each do |it|
+      @tot_linhas_por_modelo[it[:name]] = it[:data]
+    end
+
     linhas_por_tipo = []
     Funcao.select('tipo').where('cd_empresa = ?', "#{params[:cd_empresa]}").group(:tipo).each do |reg|
       Funcao.where('tipo = ? and cd_empresa = ?', reg.tipo, "#{params[:cd_empresa]}").select('codigo').each do |regt|
