aa45a1e37e671372a000aba6afbed3bdcfa8ac89
Juliano <julianog12@hotmail.com>
2021-03-03 18:03:54 -0300

    quickstart

--- config/initializers/quickstart.rb
+++ config/initializers/quickstart.rb
@@ -17,30 +17,27 @@ empresas = [1]
 #arquivos_yml.each.with_index do |arq, index|
 empresas.each do |empresa|
   #next if Rails.env == 'development'
+  dados = Configuracao.where("cd_empresa = '#{empresa}'")
   
   scheduler.cron '30 09 * * 1-5 America/Sao_Paulo' do
-    dados = Configuracao.where("cd_empresa = '#{empresa}'")
     tempresa = dados.map{ |c| [c.parametro.to_sym, c.valor] }.to_h
     tempresa[:cd_empresa] = dados.first.cd_empresa
     Processar.new(empresa)
   end
 
   scheduler.cron '15 12 * * 1-5 America/Sao_Paulo' do
-    dados = Configuracao.where("cd_empresa = '#{empresa}'")
     tempresa = dados.map{ |c| [c.parametro.to_sym, c.valor] }.to_h
     tempresa[:cd_empresa] = dados.first.cd_empresa
     Processar.new(empresa)
   end
 
   scheduler.cron '30 15 * * 1-5 America/Sao_Paulo' do
-    dados = Configuracao.where("cd_empresa = '#{empresa}'")
     tempresa = dados.map{ |c| [c.parametro.to_sym, c.valor] }.to_h
     tempresa[:cd_empresa] = dados.first.cd_empresa
     Processar.new(empresa)
   end
 
   scheduler.cron '33 17 * * 1-5 America/Sao_Paulo' do
-    dados = Configuracao.where("cd_empresa = '#{empresa}'")
     tempresa = dados.map{ |c| [c.parametro.to_sym, c.valor] }.to_h
     tempresa[:cd_empresa] = dados.first.cd_empresa
     Processar.new(empresa)
