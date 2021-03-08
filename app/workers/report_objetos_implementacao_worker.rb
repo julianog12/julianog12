113655d8d8c0b9e17a4585b3d23fd1790e88c2cd
Juliano <julianog12@hotmail.com>
2021-03-08 16:35:46 -0300

    Tirado a trigger de Exec

--- app/workers/report_objetos_implementacao_worker.rb
+++ app/workers/report_objetos_implementacao_worker.rb
@@ -6,7 +6,7 @@ class ReportObjetosImplementacaoWorker
     total_componentes = Funcao.where("cd_empresa = ? and length(cd_componente) in(7,8) and substring(cd_componente,1,4) not in ('acon', 'acre')", "#{empresa}").distinct.pluck(:cd_componente).count
     total_includes = Funcao.where("cd_empresa = ? and tipo =  'include'", "#{empresa}").count
     total_operations = Funcao.where("cd_empresa = ? and tipo =  'operation' and length(cd_componente) in(7,8) and substring(cd_componente,1,4) not in ('acon', 'acre')", "#{empresa}").count
-    total_execs = Funcao.where("cd_empresa = ? and tipo =  'trigger-form' and nm_funcao = 'EXEC' and length(cd_componente) in(7,8) and substring(cd_componente,1,4) not in ('acon', 'acre')", "#{empresa}").count
+    total_execs = 0 #Funcao.where("cd_empresa = ? and tipo =  'trigger-form' and nm_funcao = 'EXEC' and length(cd_componente) in(7,8) and substring(cd_componente,1,4) not in ('acon', 'acre')", "#{empresa}").count
 
     dados = {componentes: total_componentes, includes: total_includes, operations: total_operations, exec: total_execs}
 
