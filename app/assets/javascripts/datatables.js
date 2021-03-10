07774e8756465858d67f2048afaa8908eb6f8e14
Juliano <julianog12@hotmail.com>
2021-03-10 10:44:45 -0300

    datatables

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -5,8 +5,8 @@
 //= require datatables/dataTables.bootstrap4
 //= require datatables/extensions/Responsive/dataTables.responsive
 //= require datatables/extensions/Responsive/responsive.bootstrap4
-// require datatables/extensions/AutoFill/dataTables.autoFill
-// require datatables/extensions/AutoFill/autoFill.bootstrap4
+//= require datatables/extensions/AutoFill/dataTables.autoFill
+//= require datatables/extensions/AutoFill/autoFill.bootstrap4
 //= require datatables/extensions/Buttons/dataTables.buttons
 //= require datatables/extensions/Buttons/buttons.html5
 //= require datatables/extensions/Buttons/buttons.print
