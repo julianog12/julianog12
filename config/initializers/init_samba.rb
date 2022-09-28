daecdb4c3a34b488f756b9ce4ec5d44bb4f1f49e
Juliano Garcia <julianog12@hotmail.com>
2022-09-28 13:57:28 -0300

    Alterações nova empresa credi ctb

--- config/initializers/init_samba.rb
+++ config/initializers/init_samba.rb
@@ -5,6 +5,8 @@ if Rails.env == 'production'
   vCreDesenv = "echo Utilid@de5 | sudo -S mount -t cifs //admis16i/UnifaceDes/R97_credi_dsv/Proclisting  /var/coamo/unifacedes/r97_credi_dsv/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
   vCreProd   = "echo Utilid@de5 | sudo -S mount -t cifs //admis16i/UnifaceDes/R97_credi_pro/Proclisting  /var/coamo/unifacedes/r97_credi_pro/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
 
+  vContabCrediProd = "echo Utilid@de5 | sudo -S mount -t cifs  //admis16i/UnifaceDes/r97_crctb_pro/Proclisting /var/coamo/unifacedes/r97_crctb_pro/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
+
   vSeguradoras = "echo Utilid@de5 | sudo -S mount -t cifs //admfs03/Uniface/Coamo/Arquivos/seguradoras /var/coamo/fsuniface/coamo/arquivos/seguradoras -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
 
   system vCoDesenv
--- config/initializers/quickstart.rb
+++ config/initializers/quickstart.rb
@@ -4,11 +4,13 @@ require "#{Rails.root}/lib/processar.rb"
 require "#{Rails.root}/lib/processar_tudo.rb"
 require "#{Rails.root}/lib/gerar_relatorios_gerenciais.rb"
 
+return(0)
+
 scheduler = Rufus::Scheduler.new
 
 empresas = []
 if Rails.env == 'production'
-  empresas = [4,3,2,1]
+  empresas = [5,4,3,2] #1
   scheduler.cron '00 21 * * 1-5 America/Sao_Paulo', :job => true do
     GerarRelatoriosGerenciais.new([4,2])
   end
@@ -32,17 +34,17 @@ empresas.each do |empresa|
   tempresa = dados.map{ |c| [c.parametro.to_sym, c.valor] }.to_h
   tempresa[:cd_empresa] = dados.first.cd_empresa
 
-  scheduler.cron '30 09 * * 1-5 America/Sao_Paulo' do
-    Processar.new(tempresa)
-  end
+  #scheduler.cron '30 09 * * 1-5 America/Sao_Paulo' do
+  #  Processar.new(tempresa)
+  #end
 
   scheduler.cron '15 12 * * 1-5 America/Sao_Paulo' do
     Processar.new(tempresa)
   end
 
-  scheduler.cron '30 15 * * 1-5 America/Sao_Paulo' do
-    Processar.new(tempresa)
-  end
+  #scheduler.cron '30 15 * * 1-5 America/Sao_Paulo' do
+  #  Processar.new(tempresa)
+  #end
 
   scheduler.cron '30 19 * * 1-5 America/Sao_Paulo' do
     Processar.new(tempresa)
