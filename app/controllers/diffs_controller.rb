ae92f2184f64a70fe4e083896a8aafab6ab09e35
Juliano Garcia <julianog12@hotmail.com>
2022-12-01 14:37:33 -0300

    diff 2

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -10,30 +10,30 @@ class DiffsController < ApplicationController
   def create
     @diff = Diff.new
     if request.post?
-      vEncodeSalvo = Encoding.default_external
+      v_encode_salvo = Encoding.default_external
       require 'base64'
       if (diffs_params[:before].match(/^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=|[A-Z0-9+\/]{4})$/i))
-        vArqBefore = Base64.decode64(params[:diff][:before])
-        vArqAfter  = Base64.decode64(params[:diff][:after])
+        v_arq_before = Base64.decode64(params[:diff][:before])
+        v_arq_after  = Base64.decode64(params[:diff][:after])
 
-        Encoding.default_external = eval("Encoding::#{vArqBefore.encoding.name.gsub('-','_')}")
+        Encoding.default_external = eval("Encoding::#{v_arq_before.encoding.name.gsub('-','_')}")
 
-        params  = ActionController::Parameters.new({
+        params = ActionController::Parameters.new({
                   diff: {
-                     before: vArqBefore,
-                     after: vArqAfter
-                       }
-                 })
+                     before: v_arq_before,
+                     after: v_arq_after
+                  }
+                })
         permitted = params.require(:diff).permit(:before, :after)
-        vDados = permitted
+        v_dados = permitted
       else
-        vDados = diffs_params
+        v_dados = diffs_params
       end
 
-      @diff.attributes = vDados #diffs_params
-   end
-   render :show
-   Encoding.default_external = vEncodeSalvo
+      @diff.attributes = v_dados
+    end
+    render :show
+    Encoding.default_external = v_encode_salvo
 
   end
 
