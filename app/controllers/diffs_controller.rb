c8063c796e895c15381aba4f48278971e338513a
Juliano Garcia <julianog12@hotmail.com>
2022-12-01 14:34:54 -0300

    diff

--- app/controllers/diffs_controller.rb
+++ app/controllers/diffs_controller.rb
@@ -10,14 +10,31 @@ class DiffsController < ApplicationController
   def create
     @diff = Diff.new
     if request.post?
+      vEncodeSalvo = Encoding.default_external
       require 'base64'
       if (diffs_params[:before].match(/^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=|[A-Z0-9+\/]{4})$/i))
-        diffs_params[:before] = Base64.decode64(diffs_params[:before])
-        diffs_params[:after]  = Base64.decode64(diffs_params[:after])
+        vArqBefore = Base64.decode64(params[:diff][:before])
+        vArqAfter  = Base64.decode64(params[:diff][:after])
+
+        Encoding.default_external = eval("Encoding::#{vArqBefore.encoding.name.gsub('-','_')}")
+
+        params  = ActionController::Parameters.new({
+                  diff: {
+                     before: vArqBefore,
+                     after: vArqAfter
+                       }
+                 })
+        permitted = params.require(:diff).permit(:before, :after)
+        vDados = permitted
+      else
+        vDados = diffs_params
       end
-      @diff.attributes = diffs_params
-    end
-    render :show
+
+      @diff.attributes = vDados #diffs_params
+   end
+   render :show
+   Encoding.default_external = vEncodeSalvo
+
   end
 
   def new
