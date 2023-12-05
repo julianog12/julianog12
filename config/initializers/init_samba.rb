d0cd3313dc28ecc2b619a4470f7195d2b17786ab
Juliano Garcia <julianog12@hotmail.com>
2023-12-05 08:12:58 -0300

    init samba

--- config/initializers/init_samba.rb
+++ config/initializers/init_samba.rb
@@ -5,7 +5,7 @@ if Rails.env == 'production'
   vCreDesenv = "echo Utilid@de5 | sudo -S mount -t cifs //admis19v/CreUniDev/R10_credi_des/lst  /var/coamo/unifacedes/r10_credi_des/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
   vCreProd   = "echo Utilid@de5 | sudo -S mount -t cifs //admis19v/CreUniDev/R10_credi_pro/lst  /var/coamo/unifacedes/r10_credi_pro/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
 
-  vContabCrediProd = "echo Utilid@de5 | sudo -S mount -t cifs  //admis16i/UnifaceDes/r97_crctb_pro/Proclisting /var/coamo/unifacedes/r97_crctb_pro/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
+  #vContabCrediProd = "echo Utilid@de5 | sudo -S mount -t cifs  //admis16i/UnifaceDes/r97_crctb_pro/Proclisting /var/coamo/unifacedes/r97_crctb_pro/proclisting -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
 
   vSeguradoras = "echo Utilid@de5 | sudo -S mount -t cifs //admfs03/Uniface/Coamo/Arquivos/seguradoras /var/coamo/fsuniface/coamo/arquivos/seguradoras -o username=uniface_user,password=fatserver,workgroup=coamo,uid=root,gid=root"
 
